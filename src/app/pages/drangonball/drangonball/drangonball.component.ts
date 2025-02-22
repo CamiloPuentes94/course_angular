493c39e3bd036e42151585f519a5539f8512ad90
Camilo Puentes <jcpsandoval94@gmail.com>
2025-02-22 08:36:43 -0500

    add DrangonballComponent with basic structure

---
+++ src/app/pages/drangonball/drangonball/drangonball.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-drangonball',
+  imports: [],
+  templateUrl: './drangonball.component.html',
+})
+export class DrangonballComponent {
+
+}
