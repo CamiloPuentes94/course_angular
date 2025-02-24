19650f8aa3e409e7da9152e0ec1e734a15d953df
Camilo Puentes <jcpsandoval94@gmail.com>
2025-02-24 16:04:40 -0500

    refactor DrangonballSuperComponent to use DragonballService and remove local character management

--- src/app/pages/drangonball-super/drangonball-super.component.ts
+++ src/app/pages/drangonball-super/drangonball-super.component.ts
@@ -1,13 +1,8 @@
-import { NgClass } from '@angular/common';
-import { Component, computed, signal } from '@angular/core';
+
+import { Component, inject } from '@angular/core';
 import { CharacterListComponent } from "../../components/dragonball/character-list/character-list.component";
 import { CharacterAddComponent } from "../../components/dragonball/character-add/character-add.component";
-
-interface Character {
-  id: number;
-  name: string;
-  power: number;
-}
+import { DragonballService } from '../../services/dragonball.service';
 
 
 @Component({
@@ -17,12 +12,6 @@ interface Character {
 })
 export class DrangonballSuperComponent {
 
-  characters = signal<Character[]>([
-    { id: 1, name: 'Goku', power: 9001 },
-    { id: 2, name: 'Vegeta', power: 8000 },
-  ])
+  public dragonballService = inject(DragonballService);
 
-  addCharacter(character: Character) {
-    this.characters.update((characters) => [...characters, character]);
-  }
 }
