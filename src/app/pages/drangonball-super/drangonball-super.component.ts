8ae59db39d3e8183e8804ac954942d7938074d21
Camilo Puentes <jcpsandoval94@gmail.com>
2025-02-24 07:44:36 -0500

    add DrangonballSuperComponent to manage character input and listing

---
+++ src/app/pages/drangonball-super/drangonball-super.component.ts
@@ -0,0 +1,47 @@
+import { NgClass } from '@angular/common';
+import { Component, computed, signal } from '@angular/core';
+
+interface Character {
+  id: number;
+  name: string;
+  power: number;
+}
+
+
+@Component({
+  selector: 'drangonball-super',
+  templateUrl: './drangonball-super.component.html',
+})
+export class DrangonballSuperComponent {
+
+  name = signal('');
+  power = signal(0);
+
+
+
+  characters = signal<Character[]>([
+    { id: 1, name: 'Goku', power: 9001 },
+    { id: 2, name: 'Vegeta', power: 8000 },
+  ])
+
+  addCharacter() {
+    if(!this.name() || !this.power() || this.power() < 0) {
+      return;
+    }
+    const newCharacter = {
+      id: this.characters().length + 1,
+      name: this.name(),
+      power: this.power()
+    }
+
+    this.characters.update(
+      characters => [...characters, newCharacter]
+    )
+    this.resetFields();
+  }
+
+  resetFields() {
+    this.name.set('');
+    this.power.set(0);
+  }
+}
