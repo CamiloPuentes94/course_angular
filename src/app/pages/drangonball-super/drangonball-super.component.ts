aa7b1b19e02f1b9ae73bb1bcf6463283b7c0970b
Camilo Puentes <jcpsandoval94@gmail.com>
2025-02-24 15:54:25 -0500

    refactor DrangonballSuperComponent to integrate CharacterAddComponent and simplify character addition logic

--- src/app/pages/drangonball-super/drangonball-super.component.ts
+++ src/app/pages/drangonball-super/drangonball-super.component.ts
@@ -1,6 +1,7 @@
 import { NgClass } from '@angular/common';
 import { Component, computed, signal } from '@angular/core';
 import { CharacterListComponent } from "../../components/dragonball/character-list/character-list.component";
+import { CharacterAddComponent } from "../../components/dragonball/character-add/character-add.component";
 
 interface Character {
   id: number;
@@ -12,38 +13,16 @@ interface Character {
 @Component({
   selector: 'drangonball-super',
   templateUrl: './drangonball-super.component.html',
-  imports: [CharacterListComponent],
+  imports: [CharacterListComponent, CharacterAddComponent],
 })
 export class DrangonballSuperComponent {
-
-  name = signal('');
-  power = signal(0);
-
-
 
   characters = signal<Character[]>([
     { id: 1, name: 'Goku', power: 9001 },
     { id: 2, name: 'Vegeta', power: 8000 },
   ])
 
-  addCharacter() {
-    if(!this.name() || !this.power() || this.power() < 0) {
-      return;
-    }
-    const newCharacter = {
-      id: this.characters().length + 1,
-      name: this.name(),
-      power: this.power()
-    }
-
-    this.characters.update(
-      characters => [...characters, newCharacter]
-    )
-    this.resetFields();
-  }
-
-  resetFields() {
-    this.name.set('');
-    this.power.set(0);
+  addCharacter(character: Character) {
+    this.characters.update((characters) => [...characters, character]);
   }
 }
