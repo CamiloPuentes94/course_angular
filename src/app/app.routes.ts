fa02e72028dcc7b10f72b83e765a977315133ab1
Camilo Puentes <jcpsandoval94@gmail.com>
2025-02-24 05:57:20 -0500

    add character list component and update DrangonballComponent structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { CounterPageComponent } from './pages/counter/counter-page.component';
 import { HeroPageComponent } from './pages/hero/hero-page.component';
-import { DrangonballComponent } from './pages/drangonball/drangonball/drangonball.component';
+import { DrangonballComponent } from './pages/drangonball/drangonball.component';
 
 
 export const routes: Routes = [
