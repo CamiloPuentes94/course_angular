e6f11fa0386612fd6973a320def511bfa2065d97
Camilo Puentes <jcpsandoval94@gmail.com>
2025-02-24 07:31:25 -0500

    add functionality to add new characters and reset input fields in DrangonballComponent

--- src/app/pages/drangonball/drangonball.component.ts
+++ src/app/pages/drangonball/drangonball.component.ts
@@ -34,4 +34,25 @@ export class DrangonballComponent {
       'text-danger': true,
     }
   })
+
+  addCharacter() {
+    if(!this.name() || !this.power() || this.power() < 0) {
+      return;
+    }
+    const newCharacter = {
+      id: this.characters().length + 1,
+      name: this.name(),
+      power: this.power()
+    }
+
+    this.characters.update(
+      characters => [...characters, newCharacter]
+    )
+    this.resetFields();
+  }
+
+  resetFields() {
+    this.name.set('');
+    this.power.set(0);
+  }
 }
